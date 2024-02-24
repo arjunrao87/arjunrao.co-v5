5e7424cdc45d7a6406af5df332e9e5acfe3abb13
Arjun Rao <rao.arjun87@gmail.com>
2024-02-24 16:51:58 -0500

    fix: more analytics stuff

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,7 +9,6 @@ import Footer from '@/components/Footer'
 import siteMetadata from '@/data/siteMetadata'
 import { ThemeProviders } from './theme-providers'
 import { Metadata } from 'next'
-import Head from 'next/head'
 import Script from 'next/script'
 
 const inter = Inter({
--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,12 +1,18 @@
 import projectsData from '@/data/projectsData'
 import Card from '@/components/Card'
 import { genPageMetadata } from 'app/seo'
+import Script from 'next/script'
 
 export const metadata = genPageMetadata({ title: 'Projects' })
 
 export default function Projects() {
   return (
     <>
+      <Script
+        data-goatcounter="https://arjunraov5.goatcounter.com/count"
+        async
+        src="//gc.zgo.at/count.js"
+      ></Script>
       <div className="divide-y divide-gray-200 dark:divide-gray-700">
         <div className="space-y-2 pb-8 pt-6 md:space-y-5">
           <h1 className="prose prose-stone text-3xl leading-9 tracking-tight dark:prose-invert sm:text-3xl sm:leading-10 md:text-4xl md:leading-14">
